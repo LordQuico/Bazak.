58728d1c27e27917a93f94036464deed0888bd2c
Lord_Quico <101150623+LordQuico@users.noreply.github.com>
2022-10-10 13:01:57 +0000

    mecha

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessageReactions,
-  ],
+require('http').createServer((req, res) => res.end('Hola')).listen();
+
+/////
+
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: 32767 });
+const fs = require('fs')
+const path = require('path')
+
+const dirEvents = fs.readdirSync(path.join(__dirname, 'events'));
+
+for (const fileEvent of dirEvents) {
+  const event = require(path.join(__dirname, 'events', fileEvent));
+  client.on(event.name, (...args) => event.run(client, ...args));
+}
+
+client.on('ready', () => {
+  console.log('Toy activo prrrrr')
+  client.application.commands.set([
+    {
+      name: 'ping',
+      description: 'Pingeame esta!',
+      options: [],
+    }
+  ]);
 });
 
-client.once("ready", (bot) => {
-  console.log(`Bot: ${bot.user.username}\nStatus: ${bot.presence.status}
-Estamo activo papi`);
-});
-
-//El papu prrrr
+client.on('interactionCreate', (int) => {
+if (int.isCommand() && int.commandName === 'ping') {
+  int.reply('pong!')
+}  
+})
 
 client.login(process.env.Token);
